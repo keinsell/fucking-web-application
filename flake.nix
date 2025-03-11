6d302b8f79a46485ca4edfcecefdf0762130343d
keinsell <keinsell@protonmail.com>
2025-03-12 00:59:37 +0100

    add nix-based development environment

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.flakelight.url = "github:nix-community/flakelight";
+  inputs.flakelight-darwin.url = "github:cmacrae/flakelight-darwin";
+  outputs = {
+    flakelight,
+    flakelight-darwin,
+    ...
+  } @ inputs:
+    flakelight ./. {
+      inherit inputs;
+      imports = [flakelight-darwin.flakelightModules.default];
+      devShell.packages = pkgs: [pkgs.hello pkgs.coreutils pkgs.alejandra pkgs.deno pkgs.nodejs pkgs.ocamlPackages.reanalyze];
+
+      devShell. shellHook = ''export PATH="./node_modules/.bin:$PATH"'';
+    };
+}
