05ea5177d09371a6e7ff7715c473ead0d40a1dd4
keinsell <keinsell@protonmail.com>
2025-03-14 11:32:54 +0100

    chore: add `biome` formatter

--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 {
   inputs.flakelight.url = "github:nix-community/flakelight";
   inputs.flakelight-darwin.url = "github:cmacrae/flakelight-darwin";
-  outputs = {
-    flakelight,
-    flakelight-darwin,
-    ...
-  } @ inputs:
+  outputs =
+    { flakelight
+    , flakelight-darwin
+    , ...
+    } @ inputs:
     flakelight ./. {
       inherit inputs;
-      imports = [flakelight-darwin.flakelightModules.default];
-      devShell.packages = pkgs: [pkgs.hello pkgs.coreutils pkgs.alejandra pkgs.deno pkgs.nodejs pkgs.ocamlPackages.reanalyze pkgs.biome];
+      imports = [ flakelight-darwin.flakelightModules.default flakelight.flakelightModules.flakelightModule ];
+      devShell.packages = pkgs: [ pkgs.hello pkgs.coreutils pkgs.alejandra pkgs.deno pkgs.nodejs pkgs.ocamlPackages.reanalyze pkgs.biome ];
+
 
       devShell. shellHook = ''export PATH="./node_modules/.bin:$PATH"'';
+
+      formatters = pkgs: {
+        "*.js" = "${pkgs.biome}/bin/biome format --write";
+      };
+
     };
 }
